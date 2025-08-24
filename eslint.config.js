6fa1fe085bd4fa9bc586cc1f9b3f9b0e04d4ef36
Yushi Cui <realYushi@gmail.com>
2025-08-24 17:05:53 +1200

    feat: add ignore patterns to ESLint configuration files

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import eslintPluginAstro from "eslint-plugin-astro";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default [
+  { ignores: ["dist/", ".astro/", "node_modules/", "**/*.d.ts"] },
   ...eslintPluginAstro.configs.recommended,
   {
     rules: {
